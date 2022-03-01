b8f73fc0f709fc9e43defc618f31fd67219f5225
Tariqul Islam <usukuk@gmail.com>
2022-03-01 23:25:35 +0600

    more refactoring

--- scripts/app.js
+++ scripts/app.js
@@ -6,9 +6,7 @@ const productDetailsContainer = document.getElementById(
   "product-details-container"
 );
 
-const showSpinner = (value) => {
-  spinner.setAttribute("style", `display: ${value ? "block" : "none"}`);
-};
+let allPhones = [];
 
 // Search Handler
 searchBtn.addEventListener("click", (e) => {
@@ -21,74 +19,58 @@ searchBtn.addEventListener("click", (e) => {
 
 async function displayPhones(phoneName) {
   clearDisplay();
-  let phones = [];
+  clearShowMoreBtn();
+  allPhones = await getData(phoneName, true);
 
-  try {
-    showSpinner(true);
-    phones = await loadPhones(phoneName);
-  } catch (error) {
-    console.log(error);
-  } finally {
-    showSpinner(false);
-  }
+  // return if no data found
+  if (allPhones.length <= 0) return nothingFound();
 
-  if (phones.length <= 0) {
-    productDetailsContainer.innerHTML = `<h2 class="text-danger text-center">No phone found</h2>`;
-    return;
-  }
+  // Render Only 20 results
+  renderCards(allPhones.slice(0, 20));
 
-  phones.slice(0, 20).forEach((phone) => {
-    productsDisplay.insertAdjacentHTML(
-      "beforeend",
-
-      `<!-- Single Product -->
-    <div class="col">
-      <div class="card">
-        <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">${phone.phone_name}</h5>
-          <h6 class="card-title">${phone.brand}</h6>
-          
-          <a href="#" onclick="displayProductDetails('${phone.slug}')" class="btn btn-primary">View details</a>
-        </div>
-      </div>
-    </div>`
-    );
-  });
+  // display show more btn
+  renderShowMoreBtn();
 }
 
+// This function displays details of a product on th UI
 async function displayProductDetails(id) {
   productDetailsContainer.textContent = "";
-
-  let phone = {};
-  try {
-    showSpinner(true);
-    phone = await loadSinglePhone(id);
-  } catch (error) {
-    console.log(error);
-  } finally {
-    showSpinner(false);
-  }
-
+  let phone = await getData(id, false);
   productDetailsContainer.innerHTML = `
-    <!-- Single Product -->
-      <div class="text-center shadow shadow-lg rounded-3 py-4">
-        <h2 class="text-success">Phone Details</h2>
-        <img id="details-img" src="${phone.image}" class="w-75" alt="..." />
-        <div class="card-body">
-          <h3 class="card-title">${phone.name}</h3>
-          <h6 class="card-title">${phone.brand}</h6>
-          <h6 class="card-title">Release Date: ${
-            phone.releaseDate ? phone.releaseDate : "Not found"
-          }</h6>
-
-          ${displayMainFeatures(phone.mainFeatures)}
-
-        </div>
-      </div>`;
+    <div class="text-center shadow shadow-lg rounded-3 py-4">
+      <h2 class="text-success">Phone Details</h2>
+      <img id="details-img" src="${phone.image}" class="w-75" alt="..." />
+      <div class="card-body">
+        <h3 class="card-title">${phone.name}</h3>
+        <h6 class="card-title">${phone.brand}</h6>
+        <h6 class="card-title">Release Date: ${
+          phone.releaseDate ? phone.releaseDate : "Not found"
+        }
+        </h6>${displayMainFeatures(phone.mainFeatures)}
+      </div>
+    </div>`;
 }
 
 // utility functions
+async function getData(arg, allData) {
+  let phones = allData ? [] : {};
+  const apiLink = allData
+    ? `https://openapi.programming-hero.com/api/phones?search=${arg}`
+    : `https://openapi.programming-hero.com/api/phone/${arg}`;
+
+  try {
+    showSpinner(true);
+    const res = await fetch(apiLink);
+    const { data } = await res.json();
+    phones = data;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    showSpinner(false);
+    return phones;
+  }
+}
+
 function displayMainFeatures(features) {
   let string = "";
   for (let [key, value] of Object.entries(features)) {
@@ -98,39 +80,59 @@ function displayMainFeatures(features) {
   return string;
 }
 
-async function loadPhones(phoneName) {
-  let phones = [];
-
-  try {
-    const res = await fetch(
-      `https://openapi.programming-hero.com/api/phones?search=${phoneName}`
+function renderCards(phones) {
+  clearDisplay();
+  phones.forEach((phone) => {
+    productsDisplay.insertAdjacentHTML(
+      "beforeend",
+      `<div class="col">
+        <div class="card pt-3">
+          <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">${phone.phone_name}</h5>
+            <h6 class="card-title">${phone.brand}</h6>
+            <a href="#" onclick="displayProductDetails('${phone.slug}')" class="btn btn-primary">View details</a>
+          </div>
+        </div>
+      </div>`
     );
-    const { data } = await res.json();
-    phones = data;
-  } catch (error) {
-    console.log(error);
-  } finally {
-    return phones;
-  }
+  });
 }
 
-async function loadSinglePhone(id) {
-  let phone = {};
+function renderShowMoreBtn() {
+  const allProducts = document.getElementById("all-products");
+  allProducts.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class="text-center my-3"> 
+      <button id="show-more" type="button" class="btn btn-warning">Show More</button>
+    </div>
+    `
+  );
 
-  try {
-    const res = await fetch(
-      `https://openapi.programming-hero.com/api/phone/${id}`
-    );
-    const { data } = await res.json();
-    phone = data;
-  } catch (error) {
-    console.log(error);
-  } finally {
-    return phone;
-  }
+  // show more handler
+  const showMoreBtn = document.getElementById("show-more");
+  showMoreBtn.addEventListener("click", (e) => {
+    renderCards(allPhones);
+  });
+}
+
+function clearShowMoreBtn() {
+  const showMoreBtn = document.getElementById("show-more");
+  if (!showMoreBtn) return null;
+  showMoreBtn.parentNode.removeChild(showMoreBtn);
 }
 
 function clearDisplay() {
   productDetailsContainer.textContent = "";
   productsDisplay.textContent = "";
+}
+
+function nothingFound() {
+  productDetailsContainer.innerHTML = `<h2 class="text-danger text-center">No phone found</h2>`;
+}
+
+// to show spinner
+function showSpinner(value) {
+  spinner.setAttribute("style", `display: ${value ? "block" : "none"}`);
 }
